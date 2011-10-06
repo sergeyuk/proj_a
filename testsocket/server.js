268932caea29e8770d0bd0f604df8cf44c3afed9
sergey p <angelsyu@mail.ru>
2011-10-07 00:20:52 +0100

    change

--- testsocket/server.js
+++ testsocket/server.js
@@ -12,6 +12,9 @@ function handler (req, res) {
   });
 }
 
+var last_user_id = 0;
+var positions = {};
+
 var app = require('http').createServer(handler)
 var io = require('socket.io').listen(app)
 var fs = require('fs')
@@ -19,15 +22,30 @@ var fs = require('fs')
 console.log("start the stuff....");
 
 io.sockets.on('connection', function (socket) {
-  socket.emit('news', { msg: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-    console.log( "received other event......" )
-  });
- setInterval(1000, function(){
+	socket.broadcast.emit( 'new user' );
 
-});
+	var this_user_id = last_user_id++;
+	positions[this_user_id] = [0, 0];
+	socket.set('id', this_user_id, function(){
+		socket.get('id', function( err, id_value ){
+			socket.emit( 'connected', id_value );
+			socket.emit( 'pos', positions[id_value] );
+		});
+	});
+
+	console.log( "connected one.." );
+
+	socket.on( 'key pressed', function (data ){ 
+		console.log( "incrementing pos.." );
+		socket.get( 'id', function( err, id_value ){
+			var pos = positions[id_value];
+			pos[0]++;
+			socket.emit( 'pos', pos );
+		});
+	});
 });
 
 app.listen(8000);
+
+
 
